bf091d97a90a378269b1d09c52aa00b2ca7b071a
adokinssim-cpu <adokinssim@gmail.com>
2026-09-16 09:53:59 +0100

    Update api_constants.dart

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,9 +1,9 @@
 class ApiConstants {
   ApiConstants._();
 
-  static const String supabaseUrl = 'https://lpqafscddalkdahfkfxy.supabase.co';
+  static const String supabaseUrl = 'https://eulzqbczarltsrhtwmjl.supabase.co';
 
   static const String supabaseAnonKey = String.fromEnvironment(
-    'SUPABASE_ANON_KEY',
+    'sb_publishable_SWQzk-d_SiYfB7uE1xLyaQ_N8QJcPb1',
   );
 }
